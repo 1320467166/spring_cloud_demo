d7cc0c0bfcf55ae8f3b7150cc83b999e369ddcd1
yjs <yejisheng@one-sports.com>
2021-09-16 10:30:54 +0800

    fourth commit (discoveryClient获取微服务调用路径)

--- oder_service/src/main/java/cn/itcast/order/controller/orderController.java
+++ oder_service/src/main/java/cn/itcast/order/controller/orderController.java
@@ -2,11 +2,15 @@ package cn.itcast.order.controller;
 
 import cn.itcast.order.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/order")
@@ -14,10 +18,19 @@ public class orderController {
     //注入restTemplate对象
     @Autowired
     private RestTemplate restTemplate;
+    //注入DiscoveryClient
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @RequestMapping(value = "/buy/{id}",method = RequestMethod.GET)
     public Product findById(@PathVariable Long id){
-        Product product =
-        restTemplate.getForObject("http://localhost:9001/product/1",Product.class);
+       List<ServiceInstance> instances = discoveryClient.getInstances("service-product");
+/*        for (ServiceInstance serviceInstance:instances){
+            System.out.println(instances);
+        }*/
+        ServiceInstance serviceInstance = instances.get(0);
+
+       Product product = restTemplate.getForObject("http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/product/1",Product.class);
         return product;
     }
 }
--- oder_service/src/main/java/cn/itcast/order/orderApplication.java
+++ oder_service/src/main/java/cn/itcast/order/orderApplication.java
@@ -1,15 +1,21 @@
 package cn.itcast.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
 @EntityScan("cn.itcast.order.entity")
+@EnableEurekaClient
 public class orderApplication {
+
     /*
     *  使用spring提供的RestTemplate发送http请求
     *   1、在创建RestTemplate对象交给容器管理
